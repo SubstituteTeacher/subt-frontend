3fdccefd5d4f1c06a655de773dcc6f5175afb81f
Richardsonn1 <marcus.richardson@hotmail.se>
2022-02-04 00:34:07 +0100

    profile update

--- subt-frontend/src/Components/Profile/Profile.jsx
+++ subt-frontend/src/Components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import { Container, Col, Row, Card, Button } from "react-bootstrap";
+import { Container, Col, Row, Button } from "react-bootstrap";
 import "./Profile.css";
 
 const Profile = () => {
@@ -11,6 +11,7 @@ const Profile = () => {
       email: "asd@hotmail.com",
       phone: "072-419 22 22",
       bankAccount: "1234-12341112",
+      bank: "Glasbanken",
       username: "BengalBengan82",
       password: "asd",
     },
@@ -29,45 +30,71 @@ const Profile = () => {
 
   const renderInfo = (info, index) => {
     return (
-      <div key={index} className="m-5" style={{ textAlign: "left" }}>
+      <div key={index} style={{ textAlign: "left" }}>
         <Row>
           <Col>
-            <h4>{`Email`}</h4>
-          </Col>
-          <Col>
-            <h4>{` ${info.email}`}</h4>
+            <h4>&nbsp;&nbsp;{`${info.email}`}</h4>
           </Col>
         </Row>
         <Row>
           <Col>
-            <h4>{`Användarnamn`}</h4>
-          </Col>
-          <Col>
-            <h4>{`${info.username}`}</h4>
+            <h4>&nbsp;&nbsp;{`${info.username}`}</h4>
           </Col>
         </Row>
         <Row>
           <Col>
-            <h4>{`Bankkonto`}</h4>
-          </Col>
-          <Col>
-            <h4>{`${info.bankAccount}`}</h4>
+            <h4>&nbsp;&nbsp;{`${info.bankAccount}`}</h4>
           </Col>
         </Row>
         <Row>
           <Col>
-            <h4>{`Född`}</h4>
-          </Col>
-          <Col>
-            <h4>{`${info.age}`}</h4>
+            <h4>&nbsp;&nbsp;{`${info.bank}`}</h4>
           </Col>
         </Row>
         <Row>
           <Col>
-            <h4>{`Telefon`}</h4>
+            <h4>&nbsp;&nbsp;{`${info.age}`}</h4>
           </Col>
+        </Row>
+        <Row>
           <Col>
-            <h4>{`${info.phone}`}</h4>
+            <h4>&nbsp;&nbsp;{`${info.phone}`}</h4>
+          </Col>
+        </Row>
+      </div>
+    );
+  };
+  const renderParams = () => {
+    return (
+      <div style={{ textAlign: "end" }}>
+        <Row>
+          <Col>
+            <h4>{`Email`}&nbsp;&nbsp;</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h4>{`Användarnamn`}&nbsp;&nbsp;</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h4>{`Bankkonto`}&nbsp;&nbsp;</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h4>{`Bank`}&nbsp;&nbsp;</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h4>{`Född`}&nbsp;&nbsp;</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h4>{`Telefon`}&nbsp;&nbsp;</h4>
           </Col>
         </Row>
       </div>
@@ -76,24 +103,46 @@ const Profile = () => {
 
   return (
     <div id="profile-background">
-      <div className="profile-opacity text-white d-flex flex-column justify-content-center">
-        <Container className="text-center align-self-center" fluid>
-          <Row className="section-row first-row mx-auto">
-            <Col className="align-self-center">
-              <h1 style={{ fontSize: "60px" }}>{`Välkommen!`}</h1>
+      <div className="profile-opacity text-white align-items-center d-flex">
+        <Container
+          className="text-center h-75"
+          fluid
+          style={{ textShadow: "2px 2px black" }}
+        >
+          <Row className="mx-auto h-100">
+            <Col md={12} lg={6} className="align-self-center">
+              <Container fluid>
+                <Col>{tempInfo.map(renderName)}</Col>
+              </Container>
             </Col>
-            <Col className="align-self-center"></Col>
-          </Row>
-          <Row className="section-row mx-auto">
-            <Col className="">{tempInfo.map(renderName)}</Col>
-            <Col className="h-100">{tempInfo.map(renderInfo)}</Col>
-          </Row>
-          <Row className="section-row mx-auto">
-            <Col className="justify-content-end">
-              {/* <Button variant="primary">{`Inställningar`}</Button>
-              <Button variant="primary" className="mx-1">{`Statistik`}</Button> */}
+            <Col
+              lg={6}
+              md={12}
+              className="align-self-center d-flex"
+              style={{ flexDirection: "column" }}
+            >
+              <Col xs={12} className="mb-5 d-none d-lg-flex">
+                <Col></Col>
+                <Col style={{ textAlign: "start" }}>
+                  &nbsp;&nbsp;
+                  <Button>{`Inställningar`}</Button>
+                  &nbsp;
+                  <Button>{`Statistik`}</Button>
+                </Col>
+              </Col>
+              <Col xs={12} className="d-flex mb-5">
+                <Col xs={6}>{renderParams()}</Col>
+                <Col xs={6}>{tempInfo.map(renderInfo)}</Col>
+              </Col>
+              <Col xs={12} className="d-lg-none">
+                <Col style={{ textAlign: "center" }}>
+                  &nbsp;&nbsp;
+                  <Button>{`Inställningar`}</Button>
+                  &nbsp;
+                  <Button>{`Statistik`}</Button>
+                </Col>
+              </Col>
             </Col>
-            <Col></Col>
           </Row>
         </Container>
       </div>
