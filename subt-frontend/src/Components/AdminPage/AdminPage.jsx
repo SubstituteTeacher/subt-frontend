8d5d99a7239560549b314109158b21f31ac551b8
Denkaan <dennis.petreski@hotmail.com>
2022-02-10 11:39:31 +0100

    changed styling for retrieving user data

--- subt-frontend/src/Components/AdminPage/AdminPage.jsx
+++ subt-frontend/src/Components/AdminPage/AdminPage.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Container, Nav, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { db } from "../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
@@ -8,7 +8,6 @@ import "./AdminPage.css";
     function AdminPage() {
         const [users, setUsers] = useState([]);
         const usersCollectionRef = collection(db, "users");
-        const bool = false;
 
         useEffect(() => {
             const getUsers = async () => {
@@ -24,29 +23,63 @@ import "./AdminPage.css";
               <div className="adminpage-opacity text-white align-items-center d-flex">
               <Container className="job-container align-self-center m-5" fluid>
           <Row className="mx-auto">
-            {bool ? (
-              <></>
-            ) : (
               <Col className="todo overflow-auto" sm={12} md={6}>
                 <h1 className="text-white text-center">{`ANVÄNDARE`}</h1>
             {users.map((user) => {
                 return (
-                    <div
-                    className="text-white jobinfo-box "
-                    style={{ textShadow: "1px 1px black" }}>
-                {" "}
-                <h4>Email: {user.email}</h4>
-                <h4>Password: {user.password}</h4>
-                <h4>Role: {user.role}</h4>
-                </div>
+                    <Row className="mb-4">
+                    <Col className="text-black" xs={12}>
+                      <Card className="card-display">
+                        <Card.Body className="d-flex flex-column ">
+                          <Row>
+                            <Col className="d-none d-xl-block">{`Email`}</Col>
+                            <Col>{`Lösenord`}</Col>
+                            <Col className="d-none d-xl-block d-lg-block d-sm-block d-md-none">{`Roll`}</Col>
+                          </Row>
+                          <Row>
+                            <Col className="d-none d-xl-block">
+                              <Card.Text className="text-nowrap">{user.email}</Card.Text>
+                            </Col>
+                            <Col>
+                              <Card.Text className="text-nowrap">
+                                {user.password}
+                              </Card.Text>
+                            </Col>
+                            <Col className="d-none d-xl-block d-lg-block d-sm-block d-md-none">
+                              <Card.Text className="text-nowrap">{user.role}</Card.Text>
+                            </Col>
+                          </Row>
+            
+                          {/* <Button
+                            className="card-btn mt-3 p-0"
+                            onClick={() => {
+                              setJobinfo({
+                                title: card.title,
+                                location: card.location,
+                                schoolName: card.schoolName,
+                                class: card.class,
+                                date: card.date,
+                                timeStart: card.timeStart,
+                                timeEnd: card.timeEnd,
+                                text: card.text,
+                              });
+                              setShow(true);
+                              if (windowDimensions.width < 768) {
+                                setIsMobile(true);
+                              }
+                            }}
+                          >{`Information`}</Button> */}
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  </Row>
                 );
             })}
             </Col>
-                )}
-                </Row>
-                </Container>
-                </div>
-                </div>
+            </Row>
+            </Container>
+            </div>
+            </div>
 
         )}
 
