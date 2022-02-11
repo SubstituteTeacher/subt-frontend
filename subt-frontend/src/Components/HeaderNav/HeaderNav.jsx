86fdc1fff9a901b84834a435c0a0f8d087fed089
Denkaan <dennis.petreski@hotmail.com>
2022-02-11 15:14:02 +0100

    Admin login can see list of all users

--- subt-frontend/src/Components/HeaderNav/HeaderNav.jsx
+++ subt-frontend/src/Components/HeaderNav/HeaderNav.jsx
@@ -3,13 +3,47 @@ import { useEffect, useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { ImCross } from "react-icons/im";
 import { useUserAuth } from "../../context/UserAuthContext";
+import { db } from "../../firebase-config";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  runTransaction,
+  doc,
+} from "@firebase/firestore";
 import "./HeaderNav.css";
 
 const HeaderNav = () => {
   const [isOpen, setIsOpen] = useState(false);
   const handleOpen = () => setIsOpen((current) => !current);
-  const { logOut } = useUserAuth();
-  const bool = "admin";
+  const { logOut, user } = useUserAuth();
+  const [loading, setLoading] = useState(true);
+  const [todoCardInfo, setTodoCardInfo] = useState([]);
+
+  const getTaskItems = async () => {
+    const getPostsFromFirebase = [];
+    const querySnapshot = await getDocs(
+      query(collection(db, "users"), where("email", "==", user.email))
+    );
+    querySnapshot.forEach((doc) => {
+      getPostsFromFirebase.push({
+        ...doc.data(),
+        id: doc.id,
+      });
+    });
+
+    setTodoCardInfo(getPostsFromFirebase);
+    setLoading(false);
+    console.log(todoCardInfo);
+    return () => querySnapshot();
+  };
+
+  useEffect(() => {
+    if (loading === true) {
+      getTaskItems();
+    }
+  });
 
   const handleLogout = async () => {
     try {
@@ -72,11 +106,19 @@ const HeaderNav = () => {
                     <Col>
                       <a href="/main">{`Hem`}</a>
                     </Col>
-                    {bool? (
-                      <Col>
-                      <a href="/admin">{`Användare`}</a>
-                    </Col>
-                    ): (<></>)}
+                    {!!todoCardInfo.length ? (
+                      <>
+                        {todoCardInfo[0].role === "admin" ? (
+                          <Col>
+                            <a href="/admin">{`Användare`}</a>
+                          </Col>
+                        ) : (
+                          <></>
+                        )}
+                      </>
+                    ) : (
+                      <></>
+                    )}
                     <Col>
                       <a href="/profile">{`Profil`}</a>
                     </Col>
@@ -112,13 +154,21 @@ const HeaderNav = () => {
                 <Nav.Link href="/main">{`Hem`}</Nav.Link>
               </span>
             </Col>
-            {bool === "admin" ?(
-            <Col>
-              <span>
-                <Nav.Link href="/admin">{`Användare`}</Nav.Link>
-              </span>
-            </Col>
-            ) : (<></>)}
+            {!!todoCardInfo.length ? (
+              <>
+                {todoCardInfo[0].role === "admin" ? (
+                  <Col>
+                    <span>
+                      <Nav.Link href="/admin">{`Användare`}</Nav.Link>
+                    </span>
+                  </Col>
+                ) : (
+                  <></>
+                )}
+              </>
+            ) : (
+              <></>
+            )}
             <Col>
               <span>
                 <Nav.Link href="/profile">{`Profil`}</Nav.Link>
