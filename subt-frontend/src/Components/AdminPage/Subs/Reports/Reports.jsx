a6e0c02981ba642545659f8945f17e1c103c782b
Richardsonn1 <marcus.richardson@hotmail.se>
2022-02-16 13:14:35 +0100

    reports updated

--- subt-frontend/src/Components/AdminPage/Subs/Reports/Reports.jsx
+++ subt-frontend/src/Components/AdminPage/Subs/Reports/Reports.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Row, Nav } from "react-bootstrap";
+import { Button, Card, Col, Row, Nav, Collapse, Form } from "react-bootstrap";
 import { db } from "../../../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
@@ -8,13 +8,22 @@ import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 const TaskManager = () => {
   const [reports, setReports] = useState([]);
+  const [filterReports, setFilterReports] = useState([]);
   const [users, setUsers] = useState([]);
   const [tasks, setTasks] = useState([]);
   const [singleUser, setSingleUser] = useState();
   const [show, setShow] = useState(false);
+  const [open, setOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [itemIndex, setItemIndex] = useState(0);
+  const [isChecked, setIsChecked] = useState({
+    isDone: false,
+    isSick: false,
+    isOvertime: false,
+    none: true,
+  });
   const handleShow = () => setShow((current) => !current);
+  const handleOpen = () => setOpen((current) => !current);
 
   const getReports = async () => {
     const reportsCollectionRef = collection(db, "reports");
@@ -54,174 +63,265 @@ const TaskManager = () => {
     return users.filter((object) => object.id === id);
   };
 
+  const filterResults = (filter) => {
+    switch (filter) {
+      case "isSick":
+        setFilterReports(
+          reports.filter((object) => object.reportSick === true)
+        );
+        break;
+      case "isDone":
+        setFilterReports(
+          reports.filter((object) => object.reportSick === false)
+        );
+        break;
+      case "isOvertime":
+        setFilterReports(reports.filter((object) => object.overtime > 0));
+        break;
+      default:
+        setFilterReports([]);
+    }
+  };
+
   const renderReports = (data, index) => {
     let task = findTask(data.taskId);
     let user = findUser(data.userId);
-
     return (
-      <>
-        <Col key={index} className="d-flex text-white">
-          <Col xs={4}>
-            <h6>{`${task[0]?.class} - ${task[0]?.schoolName}` ?? ""}</h6>
-          </Col>
-          <Col xs={3}>
-            <h6>
-              {user[0]?.firstname !== undefined
-                ? `${user[0]?.firstname} ${user[0]?.surname}`
-                : "N/A"}
-            </h6>
-          </Col>
-          <Col xs={2} className="text-center">
-            <h6>{data.reportSick ? "Ja" : ""}</h6>
-          </Col>
-          <Col xs={1} className="text-center">
-            <h6> {data.overtime > 0 ? `${data.overtime}min` : ""}</h6>
-          </Col>
-          <Col className="d-flex justify-content-end">
-            <Nav.Link
-              style={{ padding: "0", color: "white" }}
-              onClick={() => {
-                setItemIndex(index);
-                setSingleUser(user);
-                handleShow();
-              }}
-            >
-              <AiFillEye size={25} />
-            </Nav.Link>
-            &nbsp;
-            <Nav.Link
-              style={{ padding: "0", color: "white" }}
-              onClick={() => {
-                setItemIndex(index);
-              }}
-            >
-              <MdDeleteOutline size={25} />
-            </Nav.Link>
-          </Col>
+      <Col key={index} className="d-flex text-white">
+        <Col xs={4}>
+          <h6>{`${task[0]?.class} - ${task[0]?.schoolName}`}</h6>
         </Col>
-      </>
+        <Col xs={3}>
+          <h6>
+            {user[0]?.firstname !== undefined
+              ? `${user[0]?.firstname} ${user[0]?.surname}`
+              : "N/A"}
+          </h6>
+        </Col>
+        <Col xs={2} className="text-center">
+          <h6>{data.reportSick ? "Ja" : ""}</h6>
+        </Col>
+        <Col xs={1} className="text-center">
+          <h6> {data.overtime > 0 ? `${data.overtime}min` : ""}</h6>
+        </Col>
+        <Col className="d-flex justify-content-end">
+          <Nav.Link
+            style={{ padding: "0", color: "white" }}
+            onClick={() => {
+              setItemIndex(index);
+              setSingleUser(user);
+              handleShow();
+            }}
+          >
+            <AiFillEye size={25} />
+          </Nav.Link>
+          &nbsp;
+          <Nav.Link
+            style={{ padding: "0", color: "white" }}
+            onClick={() => {
+              setItemIndex(index);
+            }}
+          >
+            <MdDeleteOutline size={25} />
+          </Nav.Link>
+        </Col>
+      </Col>
     );
   };
 
   return (
-    <>
-      <Row className="mx-auto">
-        {!show ? (
-          <>
-            <Col
-              xs={12}
-              style={{
-                width: "60vw",
-                margin: "auto",
-              }}
-            >
-              <h1 className="text-white text-center">{`RAPPORTER`}</h1>
-            </Col>
-            <Row className="mx-auto h-100 align-content-center">
-              <Card className="information-card">
-                <Card.Body className="text-white">
-                  <Col className="text-end">
-                    <Nav.Link style={{ padding: "0", color: "white" }}>
-                      <IoIosArrowDropdownCircle size={25} />
-                    </Nav.Link>
-                  </Col>
-                  <Col
-                    className="d-flex"
-                    style={{ borderBottom: "solid 1px" }}
-                    sm={12}
-                  >
-                    <Col xs={4}>{`Kurs & Skola`}</Col>
-                    <Col xs={3}>{`Användare`}</Col>
-                    <Col xs={2} className="text-center">{`Sjukdom`}</Col>
-                    <Col xs={1} className="text-center">{`Övertid`}</Col>
-                  </Col>
-                  <Col xs={12} className="mt-2">
-                    {reports.map(renderReports)}
-                  </Col>
-                </Card.Body>
-              </Card>
-            </Row>
-          </>
-        ) : (
-          <Col>
-            {show && itemIndex !== undefined ? (
-              <>
-                <Col
-                  xs={12}
-                  style={{
-                    width: "60vw",
-                    margin: "auto",
-                  }}
-                >
-                  <h1 className="text-white text-center">{`RAPPORTER`}</h1>
-                </Col>
-                <Row className="mx-auto h-100">
-                  <Card className="information-card">
-                    <Card.Body className="text-white">
-                      <Col>
-                        <Col xs={6}>
-                          <h1>{tasks[itemIndex].title}</h1>
-                          <h3>{`Skola: ${tasks[itemIndex].schoolName} - ${tasks[itemIndex].location}`}</h3>
-                          <h4>{`Kurs: ${tasks[itemIndex].class}`}</h4>
-                          <h4>{`Datum: ${tasks[itemIndex].date}`}</h4>
-                          <h4>{`Tid: ${tasks[itemIndex].timeStart} - ${tasks[itemIndex].timeEnd}`}</h4>
-                          <p>{`${tasks[itemIndex].text}`}</p>
-                        </Col>
-                        <br />
-                        <Col>
-                          <Col
-                            className="d-flex"
-                            style={{ borderBottom: "solid 1px" }}
-                          >
-                            <Col>{`Namn`}</Col>
-                            <Col>{`Telefon`}</Col>
-                            <Col>{`Email`}</Col>
-                            <Col>{`Sjukdom`}</Col>
-                            <Col>{`Övertid`}</Col>
-                          </Col>
-                          {!!singleUser.length ? (
-                            <Col className="d-flex">
-                              <Col>{`${singleUser[0].firstname} ${singleUser[0].surname}`}</Col>
-                              <Col>{singleUser[0].phone}</Col>
-                              <Col>{singleUser[0].email}</Col>
-                              <Col>
-                                {reports[itemIndex].reportSick
-                                  ? "Sjukanmäld"
-                                  : "Nej"}
-                              </Col>
-                              <Col>
-                                {reports[itemIndex].overtime > 0
-                                  ? `${reports[itemIndex].overtime}min`
-                                  : ""}
-                              </Col>
-                            </Col>
-                          ) : (
-                            <></>
-                          )}
-                        </Col>
-                        <br />
-                      </Col>
-                    </Card.Body>
-                    <div className="text-end">
-                      <Button
-                        className="m-1"
-                        variant="secondary"
-                        onClick={() => {
-                          setSingleUser("");
-                          handleShow();
-                        }}
-                      >{`stäng`}</Button>
-                    </div>
-                  </Card>
-                </Row>
-              </>
-            ) : (
-              <></>
-            )}
+    <Row className="mx-auto">
+      {!show ? (
+        <>
+          <Col
+            xs={12}
+            style={{
+              width: "60vw",
+              margin: "auto",
+            }}
+          >
+            <h1 className="text-white text-center">{`RAPPORTER`}</h1>
           </Col>
-        )}
-      </Row>
-    </>
+          <Row className="mx-auto h-100 align-content-center">
+            <Card className="information-card">
+              <Card.Body className="text-white">
+                <Col>
+                  <Nav.Link
+                    className="d-flex"
+                    style={{ padding: "0", color: "white" }}
+                    onClick={() => handleOpen()}
+                  >
+                    <IoIosArrowDropdownCircle size={25} />
+                    <p>{`Sortering`}</p>
+                  </Nav.Link>
+                </Col>
+                <Collapse className="p-0" in={open}>
+                  <Row>
+                    <Form>
+                      <Form.Group>
+                        <Col>
+                          <Form.Check
+                            type="checkbox"
+                            checked={isChecked.none}
+                            label={`Inget`}
+                            onChange={() => {
+                              filterResults("");
+                              setIsChecked({
+                                isSick: false,
+                                isDone: false,
+                                isOvertime: false,
+                                none: true,
+                              });
+                            }}
+                          />
+
+                          <Form.Check
+                            type="checkbox"
+                            checked={isChecked.isSick}
+                            label={`Sjukdom`}
+                            onChange={() => {
+                              filterResults("isSick");
+                              setIsChecked({
+                                isSick: true,
+                                isDone: false,
+                                isOvertime: false,
+                                none: false,
+                              });
+                            }}
+                          />
+
+                          <Form.Check
+                            type="checkbox"
+                            checked={isChecked.isDone}
+                            label={`Avsultat`}
+                            onChange={() => {
+                              filterResults("isDone");
+                              setIsChecked({
+                                isSick: false,
+                                isDone: true,
+                                isOvertime: false,
+                                none: false,
+                              });
+                            }}
+                          />
+
+                          <Form.Check
+                            type="checkbox"
+                            checked={isChecked.isOvertime}
+                            label={`Övertid`}
+                            onChange={() => {
+                              filterResults("isOvertime");
+                              setIsChecked({
+                                isSick: false,
+                                isDone: false,
+                                isOvertime: true,
+                                none: false,
+                              });
+                            }}
+                          />
+                        </Col>
+                      </Form.Group>
+                    </Form>
+                  </Row>
+                </Collapse>
+                <Col
+                  className="d-flex"
+                  style={{ borderBottom: "solid 1px" }}
+                  sm={12}
+                >
+                  <Col xs={4}>{`Kurs & Skola`}</Col>
+                  <Col xs={3}>{`Användare`}</Col>
+                  <Col xs={2} className="text-center">{`Sjukdom`}</Col>
+                  <Col xs={1} className="text-center">{`Övertid`}</Col>
+                </Col>
+                <Col xs={12} className="mt-2">
+                  {!filterReports.length
+                    ? reports.map(renderReports)
+                    : filterReports.map(renderReports)}
+                </Col>
+              </Card.Body>
+            </Card>
+          </Row>
+        </>
+      ) : (
+        <Col>
+          {show && itemIndex !== undefined ? (
+            <>
+              <Col
+                xs={12}
+                style={{
+                  width: "60vw",
+                  margin: "auto",
+                }}
+              >
+                <h1 className="text-white text-center">{`RAPPORTER`}</h1>
+              </Col>
+              <Row className="mx-auto h-100">
+                <Card className="information-card">
+                  <Card.Body className="text-white">
+                    <Col>
+                      <Col xs={6}>
+                        <h1>{tasks[itemIndex].title}</h1>
+                        <h3>{`Skola: ${tasks[itemIndex].schoolName} - ${tasks[itemIndex].location}`}</h3>
+                        <h4>{`Kurs: ${tasks[itemIndex].class}`}</h4>
+                        <h4>{`Datum: ${tasks[itemIndex].date}`}</h4>
+                        <h4>{`Tid: ${tasks[itemIndex].timeStart} - ${tasks[itemIndex].timeEnd}`}</h4>
+                        <p>{`${tasks[itemIndex].text}`}</p>
+                      </Col>
+                      <br />
+                      <Col>
+                        <Col
+                          className="d-flex"
+                          style={{ borderBottom: "solid 1px" }}
+                        >
+                          <Col>{`Namn`}</Col>
+                          <Col>{`Telefon`}</Col>
+                          <Col>{`Email`}</Col>
+                          <Col>{`Sjukdom`}</Col>
+                          <Col>{`Övertid`}</Col>
+                        </Col>
+                        {!!singleUser.length ? (
+                          <Col className="d-flex">
+                            <Col>{`${singleUser[0].firstname} ${singleUser[0].surname}`}</Col>
+                            <Col>{singleUser[0].phone}</Col>
+                            <Col>{singleUser[0].email}</Col>
+                            <Col>
+                              {reports[itemIndex].reportSick
+                                ? "Sjukanmäld"
+                                : "Nej"}
+                            </Col>
+                            <Col>
+                              {reports[itemIndex].overtime > 0
+                                ? `${reports[itemIndex].overtime}min`
+                                : "0min"}
+                            </Col>
+                          </Col>
+                        ) : (
+                          <></>
+                        )}
+                      </Col>
+                      <br />
+                    </Col>
+                  </Card.Body>
+                  <div className="text-end">
+                    <Button
+                      className="m-1"
+                      variant="secondary"
+                      onClick={() => {
+                        setSingleUser("");
+                        handleShow();
+                      }}
+                    >{`stäng`}</Button>
+                  </div>
+                </Card>
+              </Row>
+            </>
+          ) : (
+            <></>
+          )}
+        </Col>
+      )}
+    </Row>
   );
 };
 export default TaskManager;
